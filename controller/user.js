97d2b7439c597741399d8e3a5ef5d833700b7416
Edwin Rodriguez <edwinrodriguez@Edwins-MBP.attlocal.net>
2023-04-27 08:48:49 -0500

    seller backend

--- controller/user.js
+++ controller/user.js
@@ -154,4 +154,22 @@ router.get(
   })
 );
 
+//logout
+router.get("/logout", isAuthenticated,catchAsyncErrors(async(req,res,next) =>{
+  try{
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Log out Successfull"
+    });
+
+  }catch(error){
+    return next(new ErrorHandler(error.message, 500))
+  }
+}))
+
 module.exports = router;
