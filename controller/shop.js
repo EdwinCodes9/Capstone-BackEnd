e30da9163c82e3b15cad3859311b095efdad6938
Edwin Rodriguez <edwinrodriguez@Edwins-MBP.attlocal.net>
2023-05-09 15:15:08 -0500

    unsave change

--- controller/shop.js
+++ controller/shop.js
@@ -44,7 +44,7 @@ router.post("/create-shop", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `https://eshop-tutorial-cefl.vercel.app/seller/activation/${activationToken}`;
+    const activationUrl = `https://e-com-frontend-o8hh31vc8-edwincodes9.vercel.app/activation/${activationToken}`;
 
     try {
       await sendMail({
